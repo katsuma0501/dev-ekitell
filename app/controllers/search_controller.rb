0f0693e9192949c7daec9b56d1581bc7e4e73c4a
H3birth <h3.birth@gmail.com>
2017-11-05 17:54:41 +0000

    api call sample

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,62 @@
 class SearchController < ApplicationController
   def index
   	@query = params[:query]
+  	
+  	station_name = params[:query]
+  	
+  	ekispa_access_key = "LE_jemg3F9C3WEwk"
+  	
+  	ekispa_url = 'http://api.ekispert.jp/v1/json/'
+  	
+  	# station_url = ekispa_url + "staton?key=" + ekispa_access_key + "&oldName=上鯖江"
+  	
+  	# info_url = ekispa_url + "station/info?key=" + ekispa_access_key + "&code=22828&type=rail:nearrail:exit:welfare"
+  	
+  	
+  	station_param = URI.encode_www_form({key: ekispa_access_key, name: station_name})
+  	
+  	uri = URI.parse( ekispa_url + "?#{station_param}")
+    # リクエストパラメタを、インスタンス変数に格納
+    @query = uri.query
+  	
+  	# 新しくHTTPセッションを開始し、結果をresponseへ格納
+    response = Net::HTTP.start(uri.host, uri.port) do |http|
+      # 接続時に待つ最大秒数を設定
+      http.open_timeout = 5
+      # 読み込み一回でブロックして良い最大秒数を設定
+      http.read_timeout = 10
+      # ここでWebAPIを叩いている
+      # Net::HTTPResponseのインスタンスが返ってくる
+      http.get(uri.request_uri)
+    end
+    # 例外処理の開始
+    begin
+      # responseの値に応じて処理を分ける
+      case response
+      # 成功した場合
+      when Net::HTTPSuccess
+        # responseのbody要素をJSON形式で解釈し、hashに変換
+        @result = JSON.parse(response.body)
+        # 表示用の変数に結果を格納
+        @stationName = @result["Point"][0]["Station"]["Name"]
+      # 別のURLに飛ばされた場合
+      when Net::HTTPRedirection
+        @message = "Redirection: code=#{response.code} message=#{response.message}"
+      # その他エラー
+      else
+        @message = "HTTP ERROR: code=#{response.code} message=#{response.message}"
+      end
+    # エラー時処理
+    rescue IOError => e
+      @message = "e.message"
+    rescue TimeoutError => e
+      @message = "e.message"
+    rescue JSON::ParserError => e
+      @message = "e.message"
+    rescue => e
+      @message = "e.message"
+    end
+  	
   end
 
   def method_name
