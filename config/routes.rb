cc3c02c4b7330009511b89484cbfa0bd1a0bc4d3
H3birth <h3.birth@gmail.com>
2017-11-12 12:48:34 +0000

    develop

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
 	root to: 'root#index'
 
 	get '/search/:query' => 'search#index' , as: 'search'
-	get '/search?query=:word', to: redirect('/search/%{word}')
+	get '/search?query=:query' => 'search#index' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
